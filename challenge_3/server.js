6e8dbf2e94789093e1462c59a7746f9a71c8ad6a
Joshua Elder <jcelder88@gmail.com>
2018-10-17 12:08:28 -0700

    Initial commit with boilerplate

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,15 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
 
+app.use(express.static('client'));
+app.use(express.json());
+
+app.listen(port, (error) => {
+  if (error) {
+    console.error('Something went wrong\n', error);
+  } else {
+    console.log(`Express server listening on localhost:${port}/`)
+  }
+})
 
